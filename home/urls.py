a793c829f46b8f657cdc7b5b617750c2acd6c7ad
dragonwarrior1227 <vinay2210978@gmail.com>
2023-06-07 19:49:25 +0530

    debug

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
     path('chart-data', views.chart_data,name='chart_data'),
 ]
